e81f21d04e3ae789e6d44a2914bbee81b9ae2345
Andrea Ciceri <andrea.ciceri@autistici.org>
2022-12-17 13:03:39 +0100

    Keeping only substituters in `nixConfig`

--- flake.nix
+++ flake.nix
@@ -2,15 +2,8 @@
   description = "plutarch";
 
   nixConfig = {
-    # We don't use Recursive Nix yet.
-    extra-experimental-features = [ "nix-command" "flakes" "ca-derivations" "recursive-nix" ];
     extra-substituters = ["https://cache.iog.io" "https://public-plutonomicon.cachix.org" "https://mlabs.cachix.org"];
     extra-trusted-public-keys = ["hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" "public-plutonomicon.cachix.org-1:3AKJMhCLn32gri1drGuaZmFrmnue+KkKrhhubQk/CWc="];
-    allow-import-from-derivation = "true";
-    bash-prompt = "\\[\\e[0m\\][\\[\\e[0;2m\\]nix \\[\\e[0;1m\\]mlabs \\[\\e[0;93m\\]\\w\\[\\e[0m\\]]\\[\\e[0m\\]$ \\[\\e[0m\\]";
-    cores = "1";
-    max-jobs = "auto";
-    auto-optimise-store = "true";
   };
 
   inputs = {
